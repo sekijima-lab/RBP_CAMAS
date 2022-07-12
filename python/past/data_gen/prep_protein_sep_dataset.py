f69fdb8adf29dfa3252d88dfb7ad1b144159ee41
Takayuki Kimura <d@wakou.cc>
2022-07-12 14:38:21 +0900

    Update prep_protein_sep_dataset.py

--- python/past/data_gen/prep_protein_sep_dataset.py
+++ python/past/data_gen/prep_protein_sep_dataset.py
@@ -18,22 +18,22 @@ mode = "all_unknown_protein"  # all or lnc or all_unknown_protein
 # ---------------------------------------
 
 count_per_file = 5
-input = f"/Users/mac/Documents/transformer_tape_dnabert/train_dir/"
-output = f"/Users/mac/Documents/transformer_tape_dnabert/data/unknown_protein/"
+# input = f"/Users/mac/Documents/transformer_tape_dnabert/train_dir/"
+output = f"data/unknown_protein/"
 
 if mode == "all":
-    input2 = f"/Users/mac/Documents/transformer_tape_dnabert/data/clusteredRBPsuite/82/"
-    output_one = f"/Users/mac/Documents/transformer_tape_dnabert/data/known_protein/"
+    input2 = f"data/clusteredRBPsuite/82/"
+    output_one = f"data/known_protein/"
 elif mode == "lnc":
-    input2 = f"/Users/mac/Documents/transformer_tape_dnabert/data/clusteredRBPsuite_lncRNA_RNA_only/82/"
-    output_one = f"/Users/mac/Documents/transformer_tape_dnabert/data/known_protein_lncRNA/"
+    input2 = f"data/clusteredRBPsuite_lncRNA_RNA_only/82/"
+    output_one = f"data/known_protein_lncRNA/"
 else: # all_unknown_protein
-    input2 = f"/Users/mac/Documents/transformer_tape_dnabert/data/clusteredRBPsuite/82/"
-    output_one = f"/Users/mac/Documents/transformer_tape_dnabert/data/unknown_protein_all/"
+    input2 = f"data/clusteredRBPsuite/82/"
+    output_one = f"data/unknown_protein_all/"
 
 protein_cv_list_2805 = [['EIF3D', 'HNRNPL', 'BCLAF1', 'CPEB4', 'NSUN2', 'SMNDC1', 'AKAP8L', 'BCCIP', 'EIF3G', 'DDX24', 'METAP2', 'EIF4G2', 'LSM11', 'UCHL5', 'UTP18', 'AKAP1', 'SND1', 'DDX42', 'IGF2BP3', 'PTBP1', 'RBFOX2', 'GPKOW', 'SRSF7', 'STAU2', 'TAF15', 'SAFB2', 'ZC3H8', 'SF3B4', 'HNRNPA1', 'XRN2', 'SUGP2'], ['XPO5', 'LIN28B', 'NONO', 'SDAD1', 'ZNF622', 'SFPQ', 'SF3B1', 'UTP3', 'CPSF6', 'LARP7', 'NIPBL', 'ZRANB2', 'KHSRP', 'WRN', 'HNRNPC', 'SRSF1', 'EIF3H', 'ILF3', 'PPIG', 'CSTF2T', 'LARP4', 'SSB', 'AGGF1', 'U2AF2', 'SF3A3', 'YWHAG', 'FMR1', 'U2AF1', 'NCBP2', 'DKC1', 'TRA2A'], ['SERBP1', 'ZC3H11A', 'FXR2', 'FXR1', 'SUPV3L1', 'FKBP4', 'RPS11', 'GRSF1', 'ZNF800', 'SUB1', 'PPIL4', 'FUBP3', 'WDR3', 'NOLC1', 'UPF1', 'HNRNPU', 'HNRNPUL1', 'DROSHA', 'DDX52', 'QKI', 'AUH', 'TROVE2', 'DDX51', 'ABCF1', 'TBRG4', 'DDX21', 'XRCC6', 'GTF2F1', 'DGCR8', 'CDC40', 'MATR3'], ['PHF6', 'POLR2G', 'NIP7', 'WDR43', 'DHX30', 'FUS', 'MTPAP', 'NPM1', 'HNRNPK', 'SRSF9', 'PABPC4', 'PRPF4', 'DDX3X', 'RPS3', 'AATF', 'SBDS', 'DDX59', 'RBM15', 'DDX6', 'PUM2', 'EXOSC5', 'HLTF', 'RBM27', 'PUM1', 'PUS1', 'GEMIN5', 'GRWD1', 'RBM5', 'CSTF2', 'FTO', 'SLTM'], ['GNL3', 'EWSR1', 'AARS', 'NKRF', 'FAM120A', 'PCBP2', 'FASTKD2', 'EFTUD2', 'PRPF8', 'RBM22', 'IGF2BP2', 'BUD13', 'SAFB', 'TIAL1', 'NOL12', 'IGF2BP1', 'PCBP1', 'SLBP', 'AQR', 'DDX55', 'APOBEC3C', 'G3BP1', 'KHDRBS1', 'PABPN1', 'TARDBP', 'HNRNPM', 'TIA1', 'YBX3', 'RPS5', 'TNRC6A']]
 
-proseq_master = f"/Users/mac/Documents/transformer_tape_dnabert/data/proteinseqs.csv"
+proseq_master = f"data/proteinseqs.csv"
 
 # >chr12:13080717-13080852(+)
 # ACCGUAAGUCAAAGGUCAGACUGUCCAGUGGGUGAUCUCUCAAGUCGCCCGCUUGGCCUCUUCCAAGUGUACUUUACUUCCUUUCAUUCCUGCUCUAAAAC
